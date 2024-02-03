e243fed6c47b39066a65614d43b0925a10350ccc
emmanuelterah <emmanuelterah98@gmail.com>
2024-02-03 10:12:11 +0300

    Completed the assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 
 function About() {
   return (
@@ -6,7 +6,25 @@ function About() {
       <h2>About Me</h2>
       <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links />
+    </div>
+  );
+}
+
+export default About;
+*/
+
+import React from "react";
+import Links from "./Links";
+
+function About({ bio, github, linkedin }) {
+  return (
+    <div>
+      {/* Conditional rendering of the <p> tag based on the presence of 'bio' prop */}
+      {bio && <p>{bio}</p>}
+
+      {/* Pass github and linkedin as props to the Links component */}
+      <Links github={github} linkedin={linkedin} />
     </div>
   );
 }
